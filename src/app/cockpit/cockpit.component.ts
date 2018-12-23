1f13c8d473d9adf00a22c868cf917250ce572d11
Troy <Troywu9527@gmail.com>
2018-12-23 16:18:23 -0500

    Custom property event binding

---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+  @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>(); //to send the data from child to parent component
+  @Output('bpCreated') bluePrintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  newServerName = '';
+  newServerContent = '';
+  
+  constructor() {}
+  
+  onAddServer(){
+    this.serverCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
+  }
+
+  onAddBlueprint(){
+    this.bluePrintCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
+  }
+
+  ngOnInit() {
+  }
+
+}
